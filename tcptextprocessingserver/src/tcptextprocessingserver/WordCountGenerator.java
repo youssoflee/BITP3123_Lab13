4841e1f00b7bbf345e5e68ba4986177b397518f3
youssoflee <63400034+youssoflee@users.noreply.github.com>
2023-07-24 15:29:04 +0800

    Lab 13 Exercise 3 Server

---
+++ tcptextprocessingserver/src/tcptextprocessingserver/WordCountGenerator.java
@@ -0,0 +1,37 @@
+package tcptextprocessingserver;
+import java.util.Date;
+
+/**
+ * This class generates word count in a text
+ * 
+ * @author Ng Wei Hen
+ *
+ */
+public class WordCountGenerator {
+	
+	/*
+	 * default constructor
+	 */
+	public WordCountGenerator()
+	{
+		
+	}
+	/**
+	 * This method generates word count
+	 * 
+	 * @return word count in numeric
+	 */
+	public int getWordCount() {
+		
+		String input = "Hello I am Ng Wei Hen from BITS S1G1."; // The Input Words   
+		
+		 if (input == null || input.isEmpty()) {
+		      return 0;
+		    }
+
+		 String[] words = input.split("\\s+");
+		 return words.length;
+		
+	}
+	
+}
